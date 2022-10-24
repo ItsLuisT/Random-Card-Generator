a04e0f0e001bb4ae08f13f1a0509f7fef09a98fb
Luis <ltrelles39@gmail.com>
2022-10-24 17:37:10 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
-window.onload = function() {
-  //write your code here
+let suit = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+let randomsuit = Math.floor(Math.random() * suit.length);
+let suitVal = suit[randomsuit];
+document.getElementById("top-suit").innerHTML = suitVal;
+document.getElementById("bottom-suit").innerHTML = suitVal;
+let randomNumber = Math.floor(Math.random() * numbers.length);
+document.getElementById("card-number").innerHTML = numbers[randomNumber];
 
-  console.log("Hello Rigo from the console!");
-};
+if (suitVal === "&hearts;" || suitVal === "&diams;") {
+  document.getElementById("top-suit").style.color = "red";
+  document.getElementById("bottom-suit").style.color = "red";
+}
+console.log(document.getElementById("top-suit").innerHTML);
